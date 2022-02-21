6a8aa9c0a7f46d759c8ae0411033f1f1881fa8fc
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2022-02-21 14:31:20 -0500

    Add useEventListener usage

--- src/App.purs
+++ src/App.purs
@@ -2,7 +2,10 @@ module App where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Data.Nullable as Nullable
+import Debug (class DebugWarning)
+import Debug as Debug
 import Effect.Class.Console as Console
 import Foreign.Hooks (MouseEvent(..))
 import Foreign.Hooks as Foreign.Hooks
@@ -11,16 +14,23 @@ import React.Basic.Events as Events
 import React.Basic.Hooks (Component)
 import React.Basic.Hooks as Hooks
 
-mkApp :: Component Unit
+mkApp :: DebugWarning => Component Unit
 mkApp = do
   Hooks.component "App" \_ -> Hooks.do
     ref <- Hooks.useRef Nullable.null
 
     let
+      handleClickAnywhere event = do
+        Console.log ("Clicked anywhere")
+        Debug.traceM event
+
+    Foreign.Hooks.useEventListener MouseDown handleClickAnywhere Nothing
+
+    let
       handleClickOutside = Events.handler_ (Console.log "Clicked outside")
       handleClickInside = Events.handler_ (Console.log "Clicked inside")
 
-    Foreign.Hooks.useOnClickOutside ref handleClickOutside MouseDown
+    Foreign.Hooks.useOnClickOutside ref handleClickOutside MouseUp
 
     pure
       ( R.button
